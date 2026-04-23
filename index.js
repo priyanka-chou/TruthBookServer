23a317b7b827cffeace37fa59d27b52c04a2a7ec
priyanka-chou <164023075+priyanka-chou@users.noreply.github.com>
2026-04-23 10:15:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express =require('express');
+
+const server=express();
+
+server.get('/',(req,res)=>{
+    res.send('Hello World');
+}); 
+
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+});
