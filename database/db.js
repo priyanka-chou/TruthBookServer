40922662892183f43ea679747539be48521bd2b8
priyanka-chou <164023075+priyanka-chou@users.noreply.github.com>
2026-04-23 13:20:03 +0530

    add new database

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () =>{
     try{
-        await mongoose.connect('process.env.MONGO_URL');
+        await mongoose.connect(process.env.MONGO_URL);
         console.log('DB Connected');
     }
     catch(error){
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express =require('express');
-
+require('dotenv').config();
 const server=express();
 const connectDB = require('./database/db');
 
