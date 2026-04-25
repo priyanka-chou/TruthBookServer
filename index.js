7849b8b98462be048d869f880061f8833c1d8097
Harsh <harshpatle004@gmail.com>
2026-04-25 10:04:05 +0530

    addedd sum

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-const express =require('express');
+const express = require('express');
 require('dotenv').config();
-const server=express();
+
+const server = express();
 const connectDB = require('./database/db');
 const authRoutes = require("./routes/authRoute");
 
+server.use(express.json()); 
 
-server.use("/api/auth",authRoutes);
+server.use("/api/auth", authRoutes);
 
-server.get('/',(req,res)=>{
+server.get('/', (req, res) => {
     res.send('Hello World');
-}); 
+});
 
-
-server.listen(3000,()=>{
+server.listen(3000, () => {
     connectDB();
     console.log('Server is running on port 3000');
 });
- 
-
-
