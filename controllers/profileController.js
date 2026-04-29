3424179528dbb3723e03fbe82e200fd9885d91d7
Harsh <harshpatle004@gmail.com>
2026-04-29 15:02:56 +0530

    changes

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,29 +2,7 @@ const User = require("../models/User");
 
 const getUserProfile = async (req, res) =>{
     try{
-       const {userName} = req.params;
-
-       const existUserName = await  User.findOne({userName})
-       .populate("posts")
-       .populate("followers")
-       .populate("following");
-
-       (!existUserName) {
-        return res.status(400).json({
-            message :"User not found"
-        });
-       }
-
-       const response = {
-        fullName = existUserName.fullName,
-        userName =existUserName.userName,
-        bio = existUserName.bio,
-        profilePicture =existUserName.profilePicture,
-        coverPicture = existUserName.coverPicture,
-        postCount =existUserName.posts.length,
-        followingCount =existUserName.followingCount
-       }
-       
+      
 
     }
     catch{
