7733c5847bfdf6b0dc3376c70372c25241a7ea98
priya <priyaa01gautam@gmail.com>
2026-04-29 14:57:27 +0530

    made followschema

---
+++ models/Follow.js
@@ -0,0 +1,25 @@
+const mongoose= require("mongoose")
+const followSchema=new mongoose.Schema({ 
+
+   followersId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:"true",
+    index:"true"
+   } ,
+
+     followingId:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:"true",
+    index:"true"
+    } 
+
+  },{timestamps:"true"})
+
+followSchema.index(
+    {followingId:1,followersId:1},
+    {unique:"true"}
+
+)
+module.export=mongoose.model("Follow",followSchema)
