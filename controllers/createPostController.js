17185461838c1c0741a64c97e695f9e5243b448d
priyanka-chou <164023075+priyanka-chou@users.noreply.github.com>
2026-05-02 13:23:50 +0530

    add code controller

--- controllers/createPostController.js
+++ controllers/createPostController.js
@@ -1,41 +1,81 @@
+// const Post = require("../models/Post");
+// const User = require("../models/User");
+// const upload =require("../middleware/uploadMiddleware");    
+
+// const createPost = async (req, res, next) => {
+
+//     try {
+//         const { userId } = req.user.id;
+//         const { image, caption } = req.cleanedData;
+
+//         const createPost = await Post.create({
+//             userId,
+//             image,
+//             caption
+//         });
+
+//         await User.findByIdAndUpdate(userId, {
+//             $inc: { postCount: 1 }
+//         })
+
+//         return res.status(201).json({
+//             message: "Post created successfully",
+//             post: {
+//                 _id: post._id,
+//                 caption: post.caption,
+//                 image: post.image,
+//                 createdAt: post.createdAt
+
+//             }
+
+//         });
+
+//     }
+//     catch (error) {
+//         return res.status(500).json({
+//             message: " create post error"
+//         });
+//     }
+
+// }
+// module.exports = {createPost}
+
+
+
 const Post = require("../models/Post");
 const User = require("../models/User");
-const upload =require("../middleware/uploadMiddleware");    
 
-const createPost = async (req, res, next) => {
+const createPost = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { image, caption } = req.cleanedData;
 
-    try {
-        const { userId } = req.user.id;
-        const { image, caption } = req.cleanedData;
+    const newPost = await Post.create({
+      userId,
+      image,
+      caption
+    });
 
-        const createPost = await Post.create({
-            userId,
-            image,
-            caption
-        });
+    await User.findByIdAndUpdate(userId, {
+      $inc: { postCount: 1 }
+    });
 
-        await User.findByIdAndUpdate(userId, {
-            $inc: { postCount: 1 }
-        })
+    return res.status(201).json({
+      message: "Post created successfully",
+      post: {
+        _id: newPost._id,
+        caption: newPost.caption,
+        image: newPost.image,
+        createdAt: newPost.createdAt
+      }
+    });
 
-        return res.status(201).json({
-            message: "Post created successfully",
-            post: {
-                _id: post._id,
-                caption: post.caption,
-                image: post.image,
-                createdAt: post.createdAt
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "create post error"
+    });
+  }
+};
 
-            }
-
-        });
-
-    }
-    catch (error) {
-        return res.status(500).json({
-            message: " create post error"
-        });
-    }
-
-}
-module.exports = {createPost}
+module.exports = { createPost };
